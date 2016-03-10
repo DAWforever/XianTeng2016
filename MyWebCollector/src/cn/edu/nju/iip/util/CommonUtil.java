f094710d8eb565edef9c25a964fcacc722e94ec8
Qiang Wang <wqpod2g@gmail.com>
2016-03-10 15:21:08 +0800

    1.4.8

--- MyWebCollector/src/cn/edu/nju/iip/util/CommonUtil.java
+++ MyWebCollector/src/cn/edu/nju/iip/util/CommonUtil.java
@@ -1,6 +1,8 @@
 package cn.edu.nju.iip.util;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -8,6 +10,9 @@ import java.util.List;
 
 import jxl.Sheet;
 import jxl.Workbook;
+import jxl.write.Label;
+import jxl.write.WritableSheet;
+import jxl.write.WritableWorkbook;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -27,7 +32,7 @@ public class CommonUtil {
 		List<Url> url_list = new ArrayList<Url>();
 		try {
 			workbook = Workbook.getWorkbook(new File(System
-					.getProperty("user.dir") + "/resources/url_source.xls"));
+					.getProperty("user.dir") + "/resources/url_source_V3.1.xls"));
 		} catch (Exception e) {
 			logger.error("importFromXls error!", e);
 		}
@@ -44,17 +49,18 @@ public class CommonUtil {
 		workbook.close();
 		return url_list;
 	}
-	
+
 	/**
 	 * 抽取待爬取url的主域名
+	 * 
 	 * @return
 	 */
 	public static String extractSourceUrl() {
 		String source_url = null;
-		
+
 		return source_url;
 	}
-	
+
 	public static List<String> importUnitName() {
 		Workbook workbook = null;
 		List<String> list = new ArrayList<String>();
@@ -68,9 +74,30 @@ public class CommonUtil {
 		int rowCount = sheet.getRows();
 		for (int i = 1; i < rowCount; i++) {
 			String UnitName = sheet.getCell(2, i).getContents().trim();
-			if(UnitName.contains("公司")) {
+			if (UnitName.contains("公司")) {
 				list.add(UnitName);
 			}
+		}
+		return list;
+	}
+	
+	public static List<String> importUrl() {
+		Workbook workbook = null;
+		List<String> list = new ArrayList<String>();
+		try {
+			workbook = Workbook.getWorkbook(new File(System
+					.getProperty("user.dir") + "/resources/url_source_V3.1.xls"));
+		} catch (Exception e) {
+			logger.error("importFromXls error!", e);
+		}
+		Sheet sheet = workbook.getSheet(2);
+		int rowCount = sheet.getRows();
+		for (int i = 1; i < rowCount; i++) {
+			String url = sheet.getCell(4, i).getContents().trim();
+			if(url.endsWith("/")){
+				url = url.substring(0,url.length()-1);
+			}
+			list.add(url);
 		}
 		return list;
 	}
@@ -104,12 +131,49 @@ public class CommonUtil {
 		}
 	}
 
-	public static void main(String[] args) {
-		List<String> list = importUnitName();
-		for(String name:list) {
-			logger.info(name);
+	public static void readTxt() {
+		List<String> list = importUrl();
+		try {
+			WritableWorkbook book = Workbook.createWorkbook(new File(
+					"C:/Users/wangqiang/Desktop/test.xls"));
+			WritableSheet sheet = book.createSheet("第一页", 0);
+			BufferedReader in = new BufferedReader(new FileReader("resources/url.txt"));
+			String s = in.readLine();
+			int n = 0;
+			while (s != null) {
+				if (s.length() > 0) {
+					String[] str = s.split(" ");
+					String url = "http://"+str[1];
+					if(url.endsWith("/")){
+						url = url.substring(0,url.length()-1);
+					}
+					if(!list.contains(url)) {
+						String[] names = str[0].split("--");
+						String webname = names[0];
+						String press  = names[1];
+					    sheet.addCell(new Label(0, n, webname));
+					    sheet.addCell(new Label(1, n, "新闻网站"));
+					    sheet.addCell(new Label(2, n, press));
+					    sheet.addCell(new Label(3, n, url));
+					    n++;
+					}
+				}
+				s = in.readLine();
+			}
+			logger.info("n=" + n);
+			in.close();
+			// 写入数据
+			book.write();
+			// 关闭文件
+			book.close();
+		} catch (Exception e) {
+			logger.info("readTxt error", e);
 		}
-		
+	}
+	public static void main(String[] args) {
+		readTxt();
+		//importUrl();
+
 	}
 
 }
