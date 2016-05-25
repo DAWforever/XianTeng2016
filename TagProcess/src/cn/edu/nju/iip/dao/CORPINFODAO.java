69630efba60795f4f7afe8ef72189deb11896154
chenjwchenjw <chenjw@ChenJiaweideMacBook-Pro.local>
2016-05-25 17:40:45 +0800

    修改功能
    公司名称从数据库表CORPINFO中读取存入Redis

---
+++ TagProcess/src/cn/edu/nju/iip/dao/CORPINFODAO.java
@@ -0,0 +1,63 @@
+package cn.edu.nju.iip.dao;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.hibernate.Query;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+
+public class CORPINFODAO extends DAO{
+	
+	private static final Logger logger = LoggerFactory.getLogger(CORPINFODAO.class);
+	
+	public List<CORPINFO> getData(String type) {
+		List<CORPINFO> result = new ArrayList<>();
+		try{
+			begin();
+			Query q = getSession().createQuery("from CORPINFO where corp_type=:type");
+			q.setString("type", type);
+			commit();
+			Iterator<CORPINFO> it = q.iterate();
+			while(it.hasNext()){
+				CORPINFO temp = it.next();
+				//System.out.println(temp.getCorp_id() +"\t" + temp.getCorp_name() + "\t" +temp.getCorp_type());
+				result.add(temp);
+			}
+			return result;
+		}catch(Exception e) {
+			rollback();
+			logger.error("failed!",e);
+		}
+		return null;
+	}
+	
+	public String fetchID(String corp_name){
+		try{
+			begin();
+			Query q = getSession().createQuery("from CORPINFO where corp_name=:name");
+			q.setString("name", corp_name);
+			commit();
+			CORPINFO corp = (CORPINFO) q.uniqueResult();
+			return corp.getCorp_id();
+		}catch(Exception e) {
+			rollback();
+			logger.error("failed!",e);
+		}
+		return null;
+	}
+	
+	public static void main(String[] args) {
+		CORPINFODAO dao = new CORPINFODAO();
+		//ArrayList<CORPINFO> list = (ArrayList<CORPINFO>) dao.getData("3");
+		//System.out.println(list.size());
+		
+		System.out.println(dao.fetchID("重庆市万州区邮政汽车大修厂"));
+	}
+
+
+
+}
